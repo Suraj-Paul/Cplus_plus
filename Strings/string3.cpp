4360f39ec9842a0aef1e1ec5cb02019365f0b592
Suraj-Paul <paulsuraj61@gmail.com>
2020-11-29 17:32:31 +0530

    changes

--- Strings/string3.cpp
+++ Strings/string3.cpp
@@ -1,17 +1,24 @@
-#include<iostream>
-#include<string>
-#include<sstream>
+#include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 
 int main()
 {
-    string s = {"2,3,4,5,6"};
-    cout << s[1];
+    string s;
+    cout << "Enter a string with some comma's :";
+    cin >> s;
+
+    // logic to remove the "," from string
+    
     for (int i = 0; i < s.size(); i++)
     {
-        
-
+        if (int(s[i]) == 44)
+        {
+            continue;
+        }
+        cout << s[i]<<" ";
     }
-        return 0;
 
+    return 0;
 }
